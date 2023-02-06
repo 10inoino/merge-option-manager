aef33d3e492e984f5ed3236aad2573a261f4875c
10inoino <inomatahi@icloud.com>
2023-02-06 23:23:48 +0900

    first commit

---
+++ src/content.js
@@ -0,0 +1,12 @@
+const observer = new MutationObserver((mutated) => {
+    document.querySelector('.merge-message .select-menu-button').click()
+    document.querySelector('.merge-message button[value="merge"]').click()
+});
+const elem = document.getElementsByClassName('discussion-timeline-actions')[0];
+
+document.addEventListener("DOMContentLoaded", observer.observe(elem, {
+    attributes: true,
+    attributeFilter: ['svg'],
+    childList: true,
+    subtree: true
+}));

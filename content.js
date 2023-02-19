ec42052dd343ba62f08960ce1ed307c2ac5889ef
10inoino <inomatahi@icloud.com>
2023-02-19 18:57:08 +0900

    指定のmergeOptionに変更するように修正

--- content.js
+++ content.js
@@ -1,9 +1,32 @@
+let observed = false;
+
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
-  console.log("tab");
-  console.log(tab);
   const regex = /^https:\/\/github\.com\/.+\/.+\/pull\/\d+$/g;
-  if (info.status === "complete" && regex.test(tab.url)) {
-    console.log("This is pull request page");
-    // chrome.tabs.executeScript(null, { file: './contentScript.js' }, () => {});
+  if (info.status === "complete" && regex.test(tab.url) && !observed) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabId },
+        func: changeMergeOpriion,
+      });
   }
 });
+
+const changeMergeOpriion = () => {
+  var observer = new MutationObserver(() => {
+    document.querySelector(".merge-message .select-menu-button").click();
+    document.querySelector('.merge-message button[value="squash"]').click();
+    // document.querySelector('.merge-message button[value="rebase"]').click();
+    // document.querySelector('.merge-message button[value="merge"]').click();
+    observer.disconnect();
+    observed = false;
+  });
+
+  var elem = document.getElementsByClassName("discussion-timeline-actions")[0];
+  observer.observe(elem, {
+    attributes: true,
+    attributeFilter: ["svg"],
+    childList: true,
+    subtree: true,
+  });
+  observed = true;
+};

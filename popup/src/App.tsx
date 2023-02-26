3f03b100011e6bff27f87455cddbb2ca346d64a3
10inoino <inomatahi@icloud.com>
2023-02-26 21:44:40 +0900

    管理画面を作成

--- popup/src/App.tsx
+++ popup/src/App.tsx
@@ -1,12 +1,26 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import React from 'react';
 import './App.css';
 import { MergeOptionView } from './mergeOptionView';
 
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: '#333333',
+        color: 'white',
+      },
+    },
+  },
+});
+
 function App() {
   return (
-    <ChakraProvider>
-      <MergeOptionView />
-    </ChakraProvider>
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <MergeOptionView />
+      </ChakraProvider>
+    </React.StrictMode>
   );
 }
 
--- popup/src/mergeOptionView.tsx
+++ popup/src/mergeOptionView.tsx
@@ -1,7 +1,11 @@
+import { ArrowDownIcon, ArrowUpIcon, CopyIcon, DeleteIcon } from '@chakra-ui/icons';
 import {
+  Box,
   Button,
+  Center,
   Input,
   Select,
+  SimpleGrid,
   Stack,
   Table,
   TableContainer,
@@ -10,13 +14,15 @@ import {
   Th,
   Thead,
   Tr,
+  useToast,
 } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 export const MergeOptionView = () => {
-  type mergeOption = 'MERGE' | 'SQUASH' | 'REBASE';
+  const toast = useToast();
+  type mergeOption = 'MERGE' | 'SQUASH' | 'REBASE' | 'NONE';
   type mergeOptionSetting = {
-    priority: number;
+    order: number;
     owner: string;
     repo: string;
     branch: string;
@@ -24,80 +30,151 @@ export const MergeOptionView = () => {
   };
   const [mergeOptionSettings, setMergeOptionSettings] = useState<mergeOptionSetting[]>([]);
 
-  const addEmptySetting = () => {
+  const addSetting = (
+    owner: string = '*',
+    repo: string = '*',
+    branch: string = '*',
+    mergeOption: mergeOption = 'NONE'
+  ) => {
     const newMergeOptionSettings = mergeOptionSettings.concat({
-      priority: mergeOptionSettings.length + 1,
-      owner: '',
-      repo: '',
-      branch: '',
-      mergeOption: 'MERGE',
+      order: mergeOptionSettings.length + 1,
+      owner: owner,
+      repo: repo,
+      branch: branch,
+      mergeOption: mergeOption,
     });
     setMergeOptionSettings(newMergeOptionSettings);
-  }
+  };
 
-  useEffect(() => {
-    chrome.storage.sync.get(['mergeOptionSettings'], (result) => {
-      console.log('Settings retrieved', result)
-      if (result.mergeOptionSettings && result.mergeOptionSettings.length > 0) {
-        setMergeOptionSettings(result.mergeOptionSettings);
-      } else {
-        setMergeOptionSettings([
-          {
-            priority: 1,
-            owner: '',
-            repo: '',
-            branch: '',
-            mergeOption: 'MERGE',
-          }
-        ]);
-      }
-    });
-  }, []);
-
-  const saveSettings = () => {
-    chrome.storage.sync.set({ mergeOptionSettings: mergeOptionSettings }, () => {
-      console.log('Settings saved');
+  const replaceOrder = (base: number, target: number) => {
+    setMergeOptionSettings((prevMergeOptionSettings) => {
+      const newMergeOptionSettings = prevMergeOptionSettings.map((mergeOptionSetting, i) => {
+        if (mergeOptionSetting.order === base) {
+          return { ...mergeOptionSetting, order: target };
+        }
+        if (mergeOptionSetting.order === target) {
+          return { ...mergeOptionSetting, order: base };
+        }
+        return mergeOptionSetting;
+      });
+      console.log('newMergeOptionSettings', newMergeOptionSettings);
+      return newMergeOptionSettings.sort((a, b) => a.order - b.order);
     });
   };
 
-  const updateRepo = (index: number, repo: string) => {
-    const newMergeOptionSettings = mergeOptionSettings.map((mergeOptionSetting, i) => {
-      if (i === index) {
-        return { ...mergeOptionSetting, repo: repo };
-      }
-      return mergeOptionSetting;
+  const validateOwner = (owner: string) => {
+    return owner.length > 0;
+  };
+
+  const validateRepo = (repo: string) => {
+    return repo.length > 0;
+  };
+
+  const validateBranch = (branch: string) => {
+    return branch.length > 0;
+  };
+
+  const validSettings = useMemo(() => {
+    if (mergeOptionSettings.length === 0) return false;
+    return mergeOptionSettings.every((mergeOptionSetting) => {
+      return (
+        validateOwner(mergeOptionSetting.owner) &&
+        validateRepo(mergeOptionSetting.repo) &&
+        validateBranch(mergeOptionSetting.branch)
+      );
     });
-    setMergeOptionSettings(newMergeOptionSettings);
+  }, [mergeOptionSettings, validateOwner, validateRepo, validateBranch]);
+
+  // useEffect(() => {
+  //   chrome.storage.sync.get(['mergeOptionSettings'], (result) => {
+  //     console.log('Settings retrieved', result);
+  //     if (result.mergeOptionSettings && result.mergeOptionSettings.length > 0) {
+  //       setMergeOptionSettings(result.mergeOptionSettings);
+  //     } else {
+  //       setMergeOptionSettings([
+  //         {
+  //           order: 1,
+  //           owner: '*',
+  //           repo: '*',
+  //           branch: '*',
+  //           mergeOption: 'NONE',
+  //         },
+  //       ]);
+  //     }
+  //   });
+  // }, []);
+
+  // const saveSettings = () => {
+  //   chrome.storage.sync.set({ mergeOptionSettings: mergeOptionSettings }, () => {
+  //     toast({
+  //       title: 'Settings saved.',
+  //       status: 'success',
+  //       duration: 3000,
+  //       isClosable: true,
+  //     });
+  //   });
+  // };
+
+  const updateRepo = (index: number, repo: string) => {
+    setMergeOptionSettings((prevMergeOptionSettings) => {
+      const newMergeOptionSettings = prevMergeOptionSettings.map((mergeOptionSetting, i) => {
+        if (i === index) {
+          return { ...mergeOptionSetting, repo: repo };
+        }
+        return mergeOptionSetting;
+      });
+      return newMergeOptionSettings;
+    });
   };
 
   const updateBranch = (index: number, branch: string) => {
-    const newMergeOptionSettings = mergeOptionSettings.map((mergeOptionSetting, i) => {
-      if (i === index) {
-        return { ...mergeOptionSetting, branch: branch };
-      }
-      return mergeOptionSetting;
+    setMergeOptionSettings((prevMergeOptionSettings) => {
+      const newMergeOptionSettings = prevMergeOptionSettings.map((mergeOptionSetting, i) => {
+        if (i === index) {
+          return { ...mergeOptionSetting, branch: branch };
+        }
+        return mergeOptionSetting;
+      });
+      return newMergeOptionSettings;
     });
-    setMergeOptionSettings(newMergeOptionSettings);
   };
 
   const updateMergeOption = (index: number, mergeOption: mergeOption) => {
-    const newMergeOptionSettings = mergeOptionSettings.map((mergeOptionSetting, i) => {
-      if (i === index) {
-        return { ...mergeOptionSetting, mergeOption: mergeOption };
-      }
-      return mergeOptionSetting;
+    setMergeOptionSettings((prevMergeOptionSettings) => {
+      const newMergeOptionSettings = prevMergeOptionSettings.map((mergeOptionSetting, i) => {
+        if (i === index) {
+          return { ...mergeOptionSetting, mergeOption: mergeOption };
+        }
+        return mergeOptionSetting;
+      });
+      return newMergeOptionSettings;
     });
-    setMergeOptionSettings(newMergeOptionSettings);
   };
 
   const updateOwner = (index: number, owner: string) => {
-    const newMergeOptionSettings = mergeOptionSettings.map((mergeOptionSetting, i) => {
-      if (i === index) {
-        return { ...mergeOptionSetting, owner: owner };
-      }
-      return mergeOptionSetting;
+    setMergeOptionSettings((prevMergeOptionSettings) => {
+      const newMergeOptionSettings = prevMergeOptionSettings.map((mergeOptionSetting, i) => {
+        if (i === index) {
+          return { ...mergeOptionSetting, owner: owner };
+        }
+        return mergeOptionSetting;
+      });
+      return newMergeOptionSettings;
     });
-    setMergeOptionSettings(newMergeOptionSettings);
+  };
+
+  const deleteSetting = (order: number) => {
+    setMergeOptionSettings((prevMergeOptionSettings) => {
+      const newMergeOptionSettings = prevMergeOptionSettings
+        .filter((mergeOptionSetting) => mergeOptionSetting.order !== order)
+        .map((mergeOptionSetting, i) => {
+          if (mergeOptionSetting.order > order) {
+            return { ...mergeOptionSetting, order: mergeOptionSetting.order - 1 };
+          }
+          return mergeOptionSetting;
+        });
+      return newMergeOptionSettings;
+    });
   };
 
   return (
@@ -106,40 +183,58 @@ export const MergeOptionView = () => {
         <Table size="sm">
           <Thead>
             <Tr>
-              <Th>#</Th>
-              <Th>Owner</Th>
-              <Th>Repository</Th>
-              <Th>Branch</Th>
-              <Th>Merge Option</Th>
+              <Th color="white">#</Th>
+              <Th color="white" px={1}>
+                Owner
+              </Th>
+              <Th color="white" px={1}>
+                Repository
+              </Th>
+              <Th color="white" px={1}>
+                Branch
+              </Th>
+              <Th color="white" px={1}>
+                Merge Option
+              </Th>
+              <Th px={1}></Th>
+              <Th px={1}></Th>
+              <Th px={1}></Th>
+              <Th px={1}></Th>
             </Tr>
           </Thead>
           <Tbody>
             {mergeOptionSettings.map((mergeOptionSetting, i) => {
               return (
-                <Tr>
-                  <Td>{mergeOptionSetting.priority}</Td>
-                  <Td>
+                <Tr key={mergeOptionSetting.order}>
+                  <Td>{mergeOptionSetting.order}</Td>
+                  <Td px={1}>
                     <Input
+                      isInvalid={!validateOwner(mergeOptionSetting.owner)}
+                      errorBorderColor="red.500"
                       value={mergeOptionSetting.owner}
                       onChange={(e) => updateOwner(i, e.target.value)}
                       size="sm"
                     />
                   </Td>
-                  <Td>
+                  <Td px={1}>
                     <Input
+                      isInvalid={!validateRepo(mergeOptionSetting.repo)}
+                      errorBorderColor="red.500"
                       value={mergeOptionSetting.repo}
                       onChange={(e) => updateRepo(i, e.target.value)}
                       size="sm"
                     />
                   </Td>
-                  <Td>
+                  <Td px={1}>
                     <Input
+                      isInvalid={!validateBranch(mergeOptionSetting.branch)}
+                      errorBorderColor="red.500"
                       value={mergeOptionSetting.branch}
                       onChange={(e) => updateBranch(i, e.target.value)}
                       size="sm"
                     />
                   </Td>
-                  <Td>
+                  <Td px={1}>
                     <Select
                       value={mergeOptionSetting.mergeOption}
                       onChange={(e) => updateMergeOption(i, e.target.value as mergeOption)}
@@ -147,7 +242,47 @@ export const MergeOptionView = () => {
                       <option value="MERGE">MERGE</option>
                       <option value="SQUASH">SQUASH</option>
                       <option value="REBASE">REBASE</option>
+                      <option value="NONE">NONE</option>
                     </Select>
+                  </Td>
+                  <Td px={1}>
+                    {i !== 0 && (
+                      <ArrowUpIcon
+                        onClick={() => {
+                          replaceOrder(mergeOptionSetting.order, mergeOptionSetting.order - 1);
+                        }}
+                      />
+                    )}
+                  </Td>
+                  <Td px={1}>
+                    {i !== mergeOptionSettings.length - 1 && (
+                      <ArrowDownIcon
+                        onClick={() => {
+                          replaceOrder(mergeOptionSetting.order, mergeOptionSetting.order + 1);
+                        }}
+                      />
+                    )}
+                  </Td>
+                  <Td px={1}>
+                    {mergeOptionSettings.length > 1 && (
+                      <DeleteIcon
+                        onClick={() => {
+                          deleteSetting(mergeOptionSetting.order);
+                        }}
+                      />
+                    )}
+                  </Td>
+                  <Td px={1}>
+                    <CopyIcon
+                      onClick={() => {
+                        addSetting(
+                          mergeOptionSetting.owner,
+                          mergeOptionSetting.repo,
+                          mergeOptionSetting.branch,
+                          mergeOptionSetting.mergeOption
+                        );
+                      }}
+                    />
                   </Td>
                 </Tr>
               );
@@ -155,22 +290,35 @@ export const MergeOptionView = () => {
           </Tbody>
         </Table>
       </TableContainer>
-      <Button
-        colorScheme="teal"
-        onClick={() => {
-          addEmptySetting();
-        }}
-      >
-        Add
-      </Button>
-      <Button
-        colorScheme="teal"
-        onClick={() => {
-          saveSettings();
-        }}
-      >
-        Save
-      </Button>
+      <Center>
+        <SimpleGrid columns={2} spacing={5} width="100%">
+          <Box>
+            <Button
+              width="100%"
+              colorScheme="gray"
+              onClick={() => {
+                addSetting();
+              }}
+              color="black"
+            >
+              Add
+            </Button>
+          </Box>
+          <Box>
+            <Button
+              width="100%"
+              colorScheme="linkedin"
+              onClick={() => {
+                // saveSettings();
+                console.log(mergeOptionSettings);
+              }}
+              isDisabled={!validSettings}
+            >
+              Save
+            </Button>
+          </Box>
+        </SimpleGrid>
+      </Center>
     </Stack>
   );
 };
